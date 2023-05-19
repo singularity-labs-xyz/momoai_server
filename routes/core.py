2f84b0c4f24889f324237436b8f8e8dd4259ca3a
niconeven <nneven@usc.edu>
2023-05-18 23:10:44 -0700

    import AsyncIteratorCallbackHandler
    remove streaming.py

--- routes/core.py
+++ routes/core.py
@@ -7,10 +7,10 @@ from uuid import UUID
 from fastapi import APIRouter
 from fastapi.responses import StreamingResponse
 from langchain.callbacks.base import AsyncCallbackHandler
+from langchain.callbacks import AsyncIteratorCallbackHandler
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import LLMResult
 from main import chain_manager, document_manager
-from utils import streaming
 
 from momoai_core import chains, logging
 
@@ -19,12 +19,7 @@ router = APIRouter(prefix="/core")
 
 @router.get("/llm/{inp}", response_model=None)
 async def llm(inp: str):
-    handler = streaming.StreamCallbackHandler()
+    handler = AsyncIteratorCallbackHandler()
     llm_chain = chain_manager.create_custom_chain(uuid.uuid4(), chains.LLMChain, llm=ChatOpenAI(verbose=True, streaming=True, callbacks=[handler]))
-
-    return await streaming.stream(llm_chain, handler, inp)
-
-
-
-
-
+    asyncio.create_task(llm_chain.arun(inp))
+    return StreamingResponse(handler.aiter())
