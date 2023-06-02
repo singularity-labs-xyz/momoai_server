a6f48a5c0d73660bea61d50e2da3e9f6bf5008a7
Brendan <b.k.morrison2@gmail.com>
2023-06-02 14:44:26 -0700

    cr chain endpoint

--- routes/chain.py
+++ routes/chain.py
@@ -10,16 +10,39 @@ from langchain.callbacks import AsyncIteratorCallbackHandler
 from langchain.chat_models import ChatOpenAI
 from main import document_manager
 
-from momoai_core import chains, logging
+from momoai_core import chains, logging, DocumentMetadata
+
+from typing import List
 
 router = APIRouter(prefix="/chain")
 
-class Message(BaseModel):
+class LLMRequestArgs(BaseModel):
     message: str
 
 @router.post("/llm/", response_model=None)
-async def llm(message: Message = Body(...)):
+async def llm(args: LLMRequestArgs = Body(...)):
     handler = AsyncIteratorCallbackHandler()
-    llm_chain = chains.LLMChain(llm=ChatOpenAI(verbose=True, streaming=True, callbacks=[handler]))
-    asyncio.create_task(llm_chain.arun(message.message))
+    llm_chain = chains.LLMChain(llm=ChatOpenAI(verbose=True, streaming=True, callbacks=[handler], max_tokens=4000))
+    asyncio.create_task(llm_chain.arun(LLMRequestArgs.message))
+    return StreamingResponse(handler.aiter())
+
+class CRRequestArgs(BaseModel):
+    user_id: str
+    document_ids: List[str]
+    message: str
+
+@router.post("/cr/", response_model=None)
+async def cr(args: CRRequestArgs = Body(...)):
+    handler = AsyncIteratorCallbackHandler()
+    document_metadatas = [
+        DocumentMetadata(
+            id=document_id,
+            user_id=None,
+            name=None,
+        )
+        for document_id in args.document_ids
+    ]
+    retriever = document_manager.get_document_retriever(user_id=args.user_id, document_metadatas=document_metadatas)
+    cr_chain = chains.CRChain(retriever=retriever, llm=ChatOpenAI(verbose=True, streaming=True, callbacks=[handler]), max_tokens_limit=3000)
+    asyncio.create_task(cr_chain.arun({"question": args.message, "chat_history": []}))
     return StreamingResponse(handler.aiter())
--- routes/user.py
+++ routes/user.py
@@ -1,8 +1,9 @@
 import uuid
 from io import BytesIO
-from fastapi import APIRouter, File, Form, UploadFile
+from fastapi import APIRouter, File, Form, UploadFile, Body
 from main import mongo, document_manager
 from momoai_core import MongoCollections, DocumentMetadata, logging
+from pydantic import BaseModel
 
 
 router = APIRouter(prefix="/user")
@@ -15,14 +16,20 @@ def getUsers():
 def addUser(_id: str, first_name: str, last_name: str):
     mongo.insert(data={"_id": _id, "first_name": first_name, "last_name": last_name}, collection=MongoCollections.users)
 
+class UploadFileRequestArgs(BaseModel):
+    user_id: str
+    document_id: str
+
 @router.post("/upload")
 async def upload_file(file: UploadFile = File(...), user_id: str = Form(...), document_id: str = Form(...)):
     logging.info(f"Uploading file {file.filename} for user {user_id}")
     file_content = await file.read()
     print(user_id)
     file_content = BytesIO(file_content)
-    metadata = DocumentMetadata()
-    metadata.id = document_id
-    metadata.user_id = user_id
+    metadata = DocumentMetadata(
+        id=document_id,
+        user_id=user_id,
+        name=file.filename,
+    )
     document_manager.upload_document(metadata, file_content)
     logging.info(f"Uploaded file {file.filename} for user {user_id}")
